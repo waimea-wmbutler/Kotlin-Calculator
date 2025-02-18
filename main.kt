7586097b26648ca357d55ff7de59b9a9e7769f1e
waimea-wmbutler <wmbutler@waimea.school.nz>
2025-02-18 15:06:43 +1300

    Started Project

---
+++ main.kt
@@ -0,0 +1,35 @@
+fun main () {
+    val userName = getString(prompt = "Enter Your Name: ")
+    while (true) {
+        println("---------------------------------------")
+        println("Calculator 7583207583297483265375632753")
+        println("---------------------------------------")
+        println(            "+ - * - + - *")
+        println()
+        println("Hello $userName! Let's Do Some MATHS!")
+        break
+    }
+
+    val num1 = getInt(prompt = "Enter Your Name: ")
+    val num2 = getInt(prompt = "Enter Your Name: ")
+
+}
+fun getString ( prompt: String): String {
+    var userInput: String
+    while (true){
+        print(prompt)
+        userInput = readln()
+        if (userInput.isNotBlank()) break
+    }
+    return userInput
+}
+fun getInt (prompt: String):Int {
+    var intValue: Int?
+
+    while (true) {
+        val userInput = getString(prompt)
+        val intValue = userInput.toIntOrNull()
+        if (intValue != null) break
+    }
+
+}
